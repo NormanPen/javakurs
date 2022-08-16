96fb69d23bb3853de86b099a1fe8947424e3bbb9
Norman Pendzich <norman.pendzich@outlook.de>
2022-08-16 08:24:40 +0200

    Variablen

---
+++ src/main/java/variablen.java
@@ -0,0 +1,32 @@
+public class variablen {
+    public static void main(String[] args) {
+        // int 4 Byte
+        // long 8 Byte
+
+        long age = 1000000000L;
+        System.out.println(age);
+
+        short s = 12;
+        byte b = -128; // -128 .... +127
+
+
+        double d = 12.333;
+        float f = 2323;
+
+        //char
+        char sb = 'C';
+        String name = "Robert";
+
+        boolean raining = true;
+
+        int x = 112,y;
+        y = 110;
+
+        final double PI = 2.1415; // final = Konstante
+
+
+
+
+
+    }
+}
