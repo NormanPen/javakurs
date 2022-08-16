30e1be309a1f9bf21dc672c7a67e11b7f0f8f73b
Norman Pendzich <norman.pendzich@outlook.de>
2022-08-16 08:33:58 +0200

    Überladene Methoden

---
+++ src/main/java/ueberladeneMethoden.java
@@ -0,0 +1,12 @@
+public class ueberladeneMethoden {
+    public static void main(String[] args) {
+        System.out.print("Hallo");
+        System.out.print(" ");
+        System.out.print("Java");
+        System.out.println();
+        System.out.printf("Hallo%nWelt%n");
+        System.out.println("1+1=2");
+        System.out.printf("%d+%d=%d%n", 1, 1, 2);
+        System.out.printf("%s %s", "Hallo", "Welt");
+    }
+}
